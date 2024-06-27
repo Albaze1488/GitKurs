679126fb775cc2dd3d521a7c3ba3c742b97143b9
Albaze1488 <artyomstepanov48@gmail.com>
2024-06-27 10:28:20 +0300

    add Spravka.cs

---
+++ Spravka.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace AlbazKursachC_
+{
+    public partial class Spravka : Form
+    {
+        public Spravka(string imagePath)
+        {
+            InitializeComponent();
+            LoadImage(imagePath);
+        }
+
+        private void LoadImage(string imagePath)
+        {
+            try
+            {
+                pictureBox1.Image = Image.FromFile(imagePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузке изображения: " + ex.Message);
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
